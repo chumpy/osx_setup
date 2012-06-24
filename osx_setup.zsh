ddabaada9f7972e078fd48461ab7650d0eb744cc
Kevin Beddingfield <kevin.beddingfield@gmail.com>
2012-06-23 23:21:12 -0700

    first attempt

---
+++ osx_setup.zsh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+# This script sets up a fresh osx install
+# Prerequisites include:
+#			1) Command line tools for xcode
+# step 1: install homebrew
+/usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
+# step 2: go to town with some brews
+brew install mysql
+brew install git
+brew install zsh
+brew install tmux
+brew install vim
+# step 3: rvm with ruby
+curl -L https://get.rvm.io | bash -s stable --ruby
+# step 4: install dot files
+cd ~ && git clone git@gitlab.com:chumpy/dot_files.git
+ln -s ~/dot_files/.zshrc ~/.zshrc
+ln -s ~/dot_files/.tmux.config ~/.tmux.config
+ln -s ~/dot_files/.vimrc ~/.vimrc
+
